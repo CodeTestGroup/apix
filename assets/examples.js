eeed18bc3218cc88c084db5fd6c2821b222980ac
CodeTestGroup (CTG) <147273420+CodeTestGroup@users.noreply.github.com>
2025-01-05 11:09:56 +0000

    Update examples.js

--- assets/examples.js
+++ assets/examples.js
@@ -4,7 +4,7 @@
             const username = $('#loginUsername').val();
             const password = $('#loginPassword').val();
             const nextPage = $('#nextPageLogin').val();
-            const url = `api.php?mode=login&username=${username}&password=${password}&nextpage=${encodeURIComponent(nextPage)}`;
+            const url = 'http://localhost/apix/api.php?mode=login&username=${username}&password=${password}&nextpage=${encodeURIComponent(nextPage)}`;
 
             window.location.href = url;  // Redirect directly to the API URL with the parameters
         });
@@ -15,7 +15,7 @@
             const username = $('#signupUsername').val();
             const password = $('#signupPassword').val();
             const nextPage = $('#nextPageSignup').val();
-            const url = `api.php?mode=signup&username=${username}&password=${password}&nextpage=${encodeURIComponent(nextPage)}`;
+            const url = `http://localhost/apix/api.php?mode=signup&username=${username}&password=${password}&nextpage=${encodeURIComponent(nextPage)}`;
 
             window.location.href = url;  // Redirect directly to the API URL with the parameters
         });
