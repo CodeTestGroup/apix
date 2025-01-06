2987d5b429bccd100dce6d3395ce59e7d7476b42
CodeTestGroup (CTG) <147273420+CodeTestGroup@users.noreply.github.com>
2025-01-06 19:57:50 +0000

    Update login.js

--- examples/assets/login.js
+++ examples/assets/login.js
@@ -1,9 +1,5 @@
 // Get the URLSearchParams object from the current URL
 const urlParams = new URLSearchParams(window.location.search);
-
-// Extract the value of the 'url' parameter
-const $url = urlParams.get('url');
-
 // Log or use the variable
 console.log($url);
 
@@ -12,6 +8,7 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
 
             const username = encodeURIComponent(document.getElementById('username').value);
             const password = encodeURIComponent(document.getElementById('password').value);
+            const $url = urlParams.get('url');
 
             const redirectUrl = `http://localhost/apix/api.php?mode=login&username=${username}&password=${password}&nextpage=${url}`;
             window.location.href = redirectUrl;
