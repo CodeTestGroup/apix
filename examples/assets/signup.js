19ae5ab70a3cc8883f1ad6b1f6c31f095a400927
CodeTestGroup (CTG) <147273420+CodeTestGroup@users.noreply.github.com>
2025-01-07 17:57:15 +0000

    Update signup.js

--- examples/assets/signup.js
+++ examples/assets/signup.js
@@ -1,9 +1,16 @@
+// Get the URLSearchParams object from the current URL
+const urlParams = new URLSearchParams(window.location.search);
+// Log or use the variable
+console.log($url);
+
 document.getElementById('signupForm').addEventListener('submit', function(event) {
             event.preventDefault();
 
             const username = encodeURIComponent(document.getElementById('username').value);
             const password = encodeURIComponent(document.getElementById('password').value);
+                        const $url = urlParams.get('url');
 
-            const redirectUrl = `http://192.168.1.25/apix/api.php?mode=signup&username=${username}&password=${password}&nextpage=examples/full-example/login.html`;
+
+            const redirectUrl = `http://192.168.1.25/apix/api.php?mode=signup&username=${username}&password=${password}&nextpage=${url}`;
             window.location.href = redirectUrl;
         });
